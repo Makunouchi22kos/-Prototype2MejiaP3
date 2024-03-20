bc9c3a528c80ada846d3f806a014cb854af1477d
Allan Mejia-Mora <723495@student.dusd.net>
2024-03-20 11:37:50 -0700

    try

--- Prototype2MejiaP3/Assets/Scripts/PlayerController.cs
+++ Prototype2MejiaP3/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,21 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
 
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
     public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -xRange)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
@@ -27,13 +31,27 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z < zMin)
+        { transform.position = new Vector3(transform.position.x, 
+            transform.position.y, zMin);
+        }
+
+        if (transform.position.z > zMax) 
+        {
+            transform.position = new Vector3(transform.position.x, 
+            transform.position.y, zMax); 
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
         }
     }
 }
